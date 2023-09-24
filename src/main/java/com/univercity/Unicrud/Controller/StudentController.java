c5ae5aa23b880fff6a9d39562a86b2437de914ee
Akash kalamulla <akashkalamulla1@gmail.com>
2023-09-24 23:47:15 +0530

    initial commit

--- src/main/java/com/univercity/Unicrud/Controller/StudentController.java
+++ src/main/java/com/univercity/Unicrud/Controller/StudentController.java
@@ -5,6 +5,7 @@ import com.univercity.Unicrud.Service.StudentServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.convert.Jsr310Converters;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,5 +19,10 @@ public class StudentController {
     @GetMapping(path ="/students")
     public List<Student> findAllStudents(){
         return services.getAllData();
+    }
+
+    @GetMapping(path = "/student/{id}")
+    public Student getStudentById(@PathVariable int id){
+        return services.getStudentById(id);
     }
 }
--- src/main/java/com/univercity/Unicrud/Service/StudentServices.java
+++ src/main/java/com/univercity/Unicrud/Service/StudentServices.java
@@ -4,8 +4,10 @@ import com.univercity.Unicrud.Data.Student;
 import com.univercity.Unicrud.Data.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentServices {
@@ -16,5 +18,14 @@ public class StudentServices {
     public List<Student> getAllData(){
         return repository.findAll();
     }
+
+    public Student getStudentById(int id){
+        Optional<Student> student = repository.findById(id);
+        if(student.isPresent()){
+            return student.get();
+        }
+        return null;
+    }
+
 
 }
